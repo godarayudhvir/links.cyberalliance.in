528b69476d9d41352dcde8ccd5add7a303f36434
Yudhvir Singh Godara <godarayudhvir@hotmail.com>
2022-05-02 00:07:22 +0530

    Updated Chart

--- js/function.js
+++ js/function.js
@@ -1,17 +1,35 @@
 var options = {
-    chart: {
-      type: 'line'
-    },
     series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+      name: "₹",
+      data: [25800,5200,14600,20305,5500,0,0,0,0]
+  }],
+    chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
     }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Income by Month',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Dec','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
   }
-  
+  };
+
   var chart = new ApexCharts(document.querySelector("#chart"), options);
-  
   chart.render();
-  
